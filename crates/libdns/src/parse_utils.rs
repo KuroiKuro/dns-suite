911331745ac4f0cd6e459ff2c985669599dd0845
KuroiKuro <ronseahald@hotmail.com>
2023-09-13 11:10:24 +0800

    Enhance docstring for bit_parser function

--- crates/libdns/src/parse_utils.rs
+++ crates/libdns/src/parse_utils.rs
@@ -1,7 +1,9 @@
 use nom::{bits::complete::take, IResult};
 
 /// Parse `count` bits from the input. The input should be a tuple containing the
-/// input byte slice, and the offset of the slice to parse from
+/// input byte slice, and the offset of the slice to parse from. The returned value
+/// is a tuple containing a tuple of the remaining input and the current offset, and
+/// the second value in the tuple is the parsed bit value as a `u8`
 pub fn bit_parser(input: (&[u8], usize), count: usize) -> IResult<(&[u8], usize), u8> {
     take(count)(input)
 }
